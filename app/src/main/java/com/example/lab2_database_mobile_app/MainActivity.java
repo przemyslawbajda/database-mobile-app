a6675f0e3b75e4daa7f5872bd3749dccba7c93c4
bajdaprzemyslaw <przemek.android20@gmail.com>
2021-05-07 13:00:22 +0200

    add menu in MainActivity and 'Delete all data' option

--- app/src/main/java/com/example/lab2_database_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/lab2_database_mobile_app/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.lab2_database_mobile_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
         //read model from the provider
         phoneViewModel = new ViewModelProvider(this).get(PhoneViewModel.class);
 
+        //set new phoneList in the adapter when data has been changed
         phoneViewModel.getAllPhones().observe(this, phones -> {
             adapter.setPhoneList(phones);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //create menu based on xml file
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        //get id of menu item
+        int id = item.getItemId();
+
+        if(id == R.id.deleteAllOption){
+            phoneViewModel.deleteAll();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
