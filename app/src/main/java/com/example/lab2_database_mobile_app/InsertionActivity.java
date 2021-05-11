e4b2a10a38d2bd6287e6686c59ec4f41a68437bd
bajdaprzemyslaw <przemek.android20@gmail.com>
2021-05-11 16:56:19 +0200

    implement listener to cancel button and add setEditTextReferences method

--- app/src/main/java/com/example/lab2_database_mobile_app/InsertionActivity.java
+++ app/src/main/java/com/example/lab2_database_mobile_app/InsertionActivity.java
@@ -1,17 +1,50 @@
 package com.example.lab2_database_mobile_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InsertionActivity extends AppCompatActivity {
+
+    EditText manufacturerEditText;
+    EditText modelEditText;
+    EditText androidVersionEditText;
+    EditText websiteEditText;
+
+    Button cancelButton;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
 
+        setEditTextReferences();
+        setCancelButton();
 
     }
+
+    //read references to inputs
+    void setEditTextReferences(){
+        manufacturerEditText = findViewById(R.id.editTextManufacturer);
+        modelEditText = findViewById(R.id.editTextModel);
+        androidVersionEditText = findViewById(R.id.editTextAndroidVersion);
+        websiteEditText = findViewById(R.id.editTextWebsite);
+    }
+
+    void setCancelButton(){
+        cancelButton = findViewById(R.id.buttonCancel);
+
+        cancelButton.setOnClickListener(v -> {
+
+            setResult(1);
+            finish();
+        });
+    }
+
+
 
 }
